23e5179254f1bb0fdb87e2552ccee1bf08fba8b8
hyun <jjjtoy@naver.com>
2023-03-11 23:08:49 +0900

    학과게시판 틀

---
+++ src/pages/Major/MajorStyles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import { COLORS } from '../../color';
+
+export const BoardLayout = styled.div`
+    display: flex;
+    background-color: white;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const BoardTableSchema = styled.div`
+    display: flex;
+    border-radius: 5px;
+    padding: 0.8em 0;
+    background: #f1f1f1;
+`;
+
+export const TableSchemaElement = styled.div`
+    font-size: 0.8rem;
+    width: 8em;
+    text-align: center;
+    justify-content: center;
+`;
+
+export const TableTitleSchema = styled.div`
+    font-size: 0.8rem;
+    width: 100%;
+    text-align: center;
+    justify-content: center;
+`;
+
+export const BoardListLayout = styled.div`
+    width: 80%;
+`;
+
+export const BoardTitle = styled.div`
+    color: ${COLORS.logo};
+    font-size: 1.8rem;
+    font-weight: bold;
+    width: 80%;
+    padding-top: 3em;
+`;
+
+export const BoardUtilsButtonsLayout = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    width: 80%;
+    padding: 1em 0;
+`;
+
+export const WritePostButton = styled.div`
+    border-radius: 5px;
+    border: solid 0.5px gray;
+    width: 4em;
+    text-align: center;
+`;
--- src/pages/Major/index.js
+++ src/pages/Major/index.js
@@ -1,7 +1,96 @@
-import React from 'react';
+import {
+    BoardUtilsButtonsLayout,
+    WritePostButton,
+    BoardTitle,
+    BoardListLayout,
+    TableSchemaElement,
+    TableTitleSchema,
+    BoardTableSchema,
+    BoardLayout,
+} from './MajorStyles';
+import NoticeLong from '../../component/PostListElement/NoticeLong';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
+const BoardList = ({ boardList }) => {
+    return (
+        <>
+            <BoardListLayout>
+                <BoardTableSchema>
+                    <TableTitleSchema>제목</TableTitleSchema>
+                    <TableSchemaElement>작성자</TableSchemaElement>
+                    <TableSchemaElement>작성일</TableSchemaElement>
+                    <TableSchemaElement>조회수</TableSchemaElement>
+                </BoardTableSchema>
+
+                {boardList.map((boardElement) => (
+                    <NoticeLong
+                        key={boardElement.id}
+                        writerName={boardElement.author}
+                        title={boardElement.title}
+                        numberOfComment={3}
+                        createDate={boardElement.createDate}
+                        postId={boardElement.id}
+                        numberOfViews={boardElement.views}
+                    />
+                ))}
+            </BoardListLayout>
+        </>
+    );
+};
+
+const BoardUtilsButtons = ({ boardId }) => {
+    return (
+        <BoardUtilsButtonsLayout>
+            <WritePostButton onClick={() => (window.location.href = `/addpost/${boardId}`)}>글쓰기</WritePostButton>
+        </BoardUtilsButtonsLayout>
+    );
+};
 const Major = () => {
-    return <>학과게시판 화면</>;
+    const [boardList, setBoardList] = useState([]);
+    const [boardName, setBoardName] = useState('학과게시판');
+    const [majorName, setMajorName] = useState('');
+    const { board_id } = useParams();
+    // const majorId = board_id.slice(0, 3);
+    // const boardId = board_id.slice(3, 6);
+
+    // const setBoardListFromServer = () => {
+    //     axios
+    //         .get(`${process.env.REACT_APP_SERVER_URL}:8001/board/${majorId}/${boardId}/list`, {
+    //             headers: {
+    //                 email: localStorage.getItem('email'),
+    //             },
+    //         })
+    //         .then((response) => {
+    //             setBoardList(response.data.postList);
+    //             setBoardName(response.data.boardName);
+    //             setMajorName(response.data.majorName);
+    //         })
+    //         .catch((response) => {
+    //             console.log(response);
+    //             alert('접근 불가능한 페이지입니다.');
+    //             window.history.back();
+    //         });
+    // };
+
+    // useEffect(() => {
+    //     if (board_id) {
+    //         setBoardListFromServer();
+    //     }
+    // }, []);
+
+    return (
+        <>
+            <BoardLayout>
+                <BoardTitle>
+                    {boardName}{majorName}
+                </BoardTitle>
+                <BoardUtilsButtons boardId={board_id} />
+                <BoardList boardList={boardList} />
+            </BoardLayout>
+        </>
+    );
 };
 
 export default Major;
