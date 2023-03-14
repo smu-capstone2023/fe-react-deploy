3e25fa56392fb77f4500134ac954643deb8da65e
차다인 <cdin23@naver.com>
2023-03-15 00:04:49 +0900

    다인-여태까지 한것들

--- src/pages/AddPost/AddPostStyles.js
+++ src/pages/AddPost/AddPostStyles.js
@@ -1,19 +1,100 @@
 import styled from 'styled-components';
 import { COLORS } from '../../color';
 
-export const WritePostNameInputText = styled.input`
+export const AnonymousContentContainer = styled.p`
+    color: gray;
+    font-size: large;
+    float: left;
+    margin-top: 0.4em;
+    margin-left: 0.5em;
+`;
+
+export const AnonymousCheckButtonContainer = styled.input`
+    zoom: 1.5;
     border-radius: 5px;
     border: solid 0.5px gray;
+    margin-top: 0.5em;
+    float: left;
+`;
+
+export const AddFileButtonContainer = styled.button`
+    width: 10em;
     height: 3em;
+    background: lightgray;
+    border: none;
+    border-radius: 5px;
+    margin-left: 20em;
+    float: left;
+`;
+
+// export const SelectHashtegLayout = styled.select`
+//     width: 30%;
+//     border-radius: 5px;
+//     border: solid 0.5px gray;
+//     height: 3.4em;
+//     padding: 1em;
+//     margin-right: 1em;
+//     float: left;
+// `;
+
+export const WritePostUserLayout = styled.div`
+    border-radius: 5px;
+    // border: solid 0.5px gray;
+    height: 4em;
+    color: gray;
+`;
+
+export const WritePostUserImageLayout = styled.img`
+    width: 3em;
+    height: 3em;
+    border: solid 0.3px lightgray;
+    border-radius: 70%;
+    overflow: hidden;
+    object-fit: cover;
+    float: left;
+`;
+
+export const WritePostUserNameLayout = styled.p`
+    color: gray;
+    padding-bottom: 0.3em;
+    padding-left: 4em;
+`;
+
+export const WritePostDateLayout = styled.p`
+    color: gray;
+    padding-left: 4.6em;
+    font-size: 14px;
+`;
+
+export const WritePostNameInputText = styled.input`
+    width: 100%;
+    border-radius: 5px;
+    border: solid 0.5px lightgray;
+    height: 4em;
     padding: 1em;
     font-weight: bold;
 `;
 
 export const WritePostContentInputText = styled.textarea`
     border-radius: 5px;
-    border: solid 0.5px gray;
+    border: solid 0.5px lightgray;
     height: 25em;
     padding: 1em;
+    overflow: auto;
+    resize: none;
+    &::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+        border-radius: 6px;
+        background: rgba(255, 255, 255, 0.4);
+    }
+    &::-webkit-scrollbar-thumb {
+        background: rgba(0, 0, 0, 0.3);
+        border-radius: 6px;
+    }
+    &::-focus {
+        outline: 1px solid gray;
+    }
 `;
 
 export const AddPostLayout = styled.div`
@@ -35,6 +116,7 @@ export const HideWriterAndCompleteButtonLayout = styled.div`
 `;
 
 export const CompletePostButtonContainer = styled.button`
+    min-width: 5em;
     width: 10em;
     height: 3em;
     background: ${COLORS.logo};
--- src/pages/AddPost/index.js
+++ src/pages/AddPost/index.js
@@ -7,10 +7,71 @@ import {
     AddPostLayout,
     WritePostNameInputText,
     WritePostContentInputText,
+    WritePostUserLayout,
+    SelectHashtagLayout,
+    AddFileButtonContainer,
+    AnonymousCheckButtonContainer,
+    AnonymousContentContainer,
+    WritePostUserImageLayout,
+    WritePostUserNameLayout,
+    WritePostDateLayout
 } from './AddPostStyles';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+import { Navigate, useParams } from 'react-router-dom';
 
+
+// 작성자 닉네임, 작성 날짜, 선택 해시태그, 첨부파일 데이터 저장
+
+
+const WriteUserField = ({setPostDate}) => {
+    const userName = localStorage.nickname;
+    let date = new Date();
+    const year = date.toLocaleString('ko-KR', {year: "numeric"});
+    const month = date.toLocaleString('ko-KR', {month: 'long'});
+    const day = date.toLocaleString('ko-KR', {day: '2-digit'});
+    const hours = date.getHours();
+    const minute = date.getMinutes();
+    
+    return (
+        <WritePostUserLayout>
+            <WritePostUserImageLayout src='https://media.istockphoto.com/id/1197796372/ko/%EB%B2%A1%ED%84%B0/%EC%82%AC%EB%9E%8C-%EB%B2%A1%ED%84%B0-%EC%95%84%EC%9D%B4%EC%BD%98%EC%9E%85%EB%8B%88%EB%8B%A4-%EC%82%AC%EB%9E%8C-%EC%95%84%EC%9D%B4%EC%BD%98.jpg?s=612x612&w=0&k=20&c=O4BhlKJtKHevLMEJqMIim3IKseu5lEYXBOm3uI8r_vk='></WritePostUserImageLayout>
+            <WritePostUserNameLayout>{userName}</WritePostUserNameLayout>
+            <WritePostDateLayout>{year} {month} {day}  {hours}:{minute}</WritePostDateLayout>
+        </WritePostUserLayout>
+    );
+};
+
+const SelectHashtagField = ({setPostHashtag}) => {
+    function handleSelect(data) {
+        setSelectedOptions(data);
+    }
+    const [selectedOptions, setSelectedOptions] = useState();
+    const HashtagOption = [
+        { label: '진로', value: '진로' },
+        { label: '학교생활', value: '학교생활' },
+        { label: '과제', value: '과제' },
+        { label: '선택 안함', value: '선택 안함' },
+    ];
+
+    const SelectHashtagLayout = {
+        container: styles => ({...styles, width: 300, height: 50})
+    }
+
+    return (
+        <Select style={{width:'20%'}}
+            options={HashtagOption}
+            placeholder="#해시태그를 선택해주세요"
+            value={selectedOptions}
+            onChange={(e) => setPostHashtag(e.target.value)}
+            isSearchable={true}
+            styles={SelectHashtagLayout}
+        />
+    );
+};
+
+// + 입력칸 클릭시 테두리 색 변화 방지
 const WritePostNameField = ({ setPostTitle }) => {
     return (
         <>
@@ -27,21 +88,55 @@ const WritePostContentField = ({ setPostContent }) => {
     );
 };
 
+const AnonymousCheckButton = ({setPostAnonymous}) => {
+    return (
+        <>
+            <AnonymousCheckButtonContainer type="Checkbox" onChange={(e) => setPostAnonymous(e.target.checked)}></AnonymousCheckButtonContainer>
+            <AnonymousContentContainer>익명</AnonymousContentContainer>
+        </>
+        
+    );
+};
+
+const AddFileButton = ({setPostAddFile}) => {
+    return (
+        <>
+            <AddFileButtonContainer>
+                첨부파일
+            </AddFileButtonContainer>
+        </>
+    );
+};
+
 const HideWriterNameToggle = ({ setHideWriterName, hideWriterName }) => {
     return <></>;
 };
 
-const CompletePostButton = ({ savePostInServer }) => {
+
+
+const CompletePostButton = ({ savePostInServer, majorId, boardId }) => {
+    function handleClick(e) {
+        window.location.href = "/board/" + majorId + boardId;
+    }
+    
     return (
         <>
-            <CompletePostButtonContainer type='submit' onClick={savePostInServer}>
-                저장하기
+            <CompletePostButtonContainer type='submit' onClick={()=> {
+                savePostInServer();
+                alert('게시물이 업로드되었습니다.');
+                handleClick();
+            }}>
+                게시하기
             </CompletePostButtonContainer>
         </>
     );
 };
 
 const AddPost = () => {
+    const [postDate, setPostDate] = useState('');
+    const [postHashtag, setPostHashtag] = useState('');
+    const [postAnonymous, setPostAnonymous] = useState('');
+    const [postAddFile, setPostAddFile] = useState();
     const [postTitle, setPostTitle] = useState('');
     const [postContent, setPostContent] = useState('');
     const { board_id } = useParams();
@@ -79,17 +174,26 @@ const AddPost = () => {
             });
     };
 
+    
     return (
         <>
             <AddPostLayout>
                 <WritePostContainer>
-                    <WritePostNameField setPostTitle={setPostTitle} />
+                    <WriteUserField></WriteUserField>
+                    <div>
+                        {/* <SelectHashtagField setPostHashtag={setPostHashtag}></SelectHashtagField> */}
+                        <WritePostNameField setPostTitle={setPostTitle} />
+                    </div>
                     <Blank1em />
                     <WritePostContentField setPostContent={setPostContent} />
                     <Blank1em />
-                    <HideWriterAndCompleteButtonLayout>
-                        <CompletePostButton savePostInServer={savePostInServer} />
-                    </HideWriterAndCompleteButtonLayout>
+                    <div>
+                        <AnonymousCheckButton setPostAnonymous={setPostAnonymous}></AnonymousCheckButton>
+                        <AddFileButton></AddFileButton>
+                        <HideWriterAndCompleteButtonLayout>
+                            <CompletePostButton savePostInServer={savePostInServer} majorId={majorId} boardId={boardId}/>
+                        </HideWriterAndCompleteButtonLayout>
+                    </div>
                 </WritePostContainer>
             </AddPostLayout>
         </>
