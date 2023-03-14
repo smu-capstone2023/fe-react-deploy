62185eb937efc35839e4af9f8b668f24333b27b4
Seyounie <aras3640@gmail.com>
2023-03-15 00:08:22 +0900

    branch test

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import AddPost from './pages/AddPost';
 import Board from './pages/Board';
 import ViewPost from './pages/ViewPost';
 import EditPost from './pages/EditPost';
+import SignupSection3 from './pages/Signup/SignupSection3';
+
+
 
 function App() {
     //TODO: 지우기
@@ -21,17 +24,21 @@ function App() {
     };
     return (
         <div>
+            //header을 따로 빼놓으심 
             <Sidebar toggle={toggle} isOpen={isOpen} setIsOpen={setIsOpen} isLogin={localStorage.getItem('email')} />
             <Navbar toggle={toggle} isOpen={isOpen} />
+
             <Routes>
                 <Route path='/' element={<Home />}></Route>
                 <Route path='/login' element={<Login />}></Route>
                 <Route path='/mypage' element={<Mypage />}></Route>
                 <Route path='/signup' element={<Signup />}></Route>
+                <Route path='/signupsession3' element={<SignupSection3 />}></Route>
                 <Route path='/addpost/:board_id' element={<AddPost />}></Route>
                 <Route path='/board/:board_id' element={<Board />}></Route>
                 <Route path='/viewpost/:post_id' element={<ViewPost />}></Route>
                 <Route path='/editpost/:post_id' element={<EditPost />}></Route>
+                //board,post id 값 주기
             </Routes>
         </div>
     );
--- src/pages/Signup/SignupSection3.js
+++ src/pages/Signup/SignupSection3.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { ProfileImage, Line, SignContainer, SignInputText, SignInnerBox, SignButton, DefaultText, SmallText } from './SignupStyles.js';
+import { Link, useNavigate } from 'react-router-dom'
+import { SignContainer, SignInputText, SignInnerBox, SignButton, DefaultText, SmallText } from './SignupStyles.js';
+
 
 const SignupSection3 = ({ userSignupInfo }) => {
     const [nickName, setNickName] = useState('');
 
+    //닉네임 입력&&등록
     const checkAllOfSingUpInfo = () => {
         if (nickName === '') {
             alert('닉네임을 입력해주세요');
@@ -15,7 +18,7 @@ const SignupSection3 = ({ userSignupInfo }) => {
         }
     };
 
-    const saveUserInfoAtServer = () => {
+const saveUserInfoAtServer = () => {
         axios
             .post(`${process.env.REACT_APP_SERVER_URL}:8001/auth/join`, {
                 email: userSignupInfo.email,
@@ -30,29 +33,22 @@ const SignupSection3 = ({ userSignupInfo }) => {
             .catch((response) => console.log(response));
     };
 
+
     return (
         <SignContainer>
             <SignInnerBox>
                 <DefaultText>사용하실 닉네임을 입력하세요</DefaultText>
                 <SmallText>닉네임은 설정 후 30일 이후에 변경 가능합니다.</SmallText>
                 <SignInputText onChange={(e) => setNickName(e.target.value)} placeholder='닉네임 입력'></SignInputText>
-                <Line></Line>
-                <DefaultText>프로필 사진을 첨부해주세요.</DefaultText>
-                <form>
-                    <ProfileImage style={{ margin: '10px' }}></ProfileImage>
-                    {/* 연결x */}
-                    <label className='signup-profileImg-label' htmlFor='profileImg'>
-                        사진 첨부하기
-                    </label>
-                    <input className='signup-profileImg-input' type='file' accept='image/*' id='profileImg' style={{ display: 'none' }} />
-                </form>
             </SignInnerBox>
+            
+            <SignButton onClick={() => {if (checkAllOfSingUpInfo()) saveUserInfoAtServer()}}>회원가입 완료!</SignButton>
             <SignButton
                 onClick={() => {
                     if (checkAllOfSingUpInfo()) saveUserInfoAtServer();
                 }}
             >
-                확인
+                회원가입 완료! 
             </SignButton>
         </SignContainer>
     );
