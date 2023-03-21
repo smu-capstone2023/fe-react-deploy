45cd2eeddccd2f9f874b8d9129d8920ede302b92
차다인 <cdin23@naver.com>
2023-03-21 15:49:20 +0900

    contents/comment 줄바꿈 수정

--- src/pages/ViewPost/index.js
+++ src/pages/ViewPost/index.js
@@ -97,21 +97,11 @@ const WriterUserInfoBlock = ({ writerName, createDate, postId, deletePost, profi
                     <ViewPostMenu writerName={writerName} postId={postId} deletePost={deletePost}></ViewPostMenu>
                 </ViewPostMenuContainer>
             </WriterUserInfoLayout>
-
-
         </>
 
     );
 };
 
-// const EditPostButton = ({ isShow, postId, writerName}) => {
-//     const userName = localStorage.nickname;
-//     if (isShow && (writerName == userName)) {
-//         return <EditPostButtonField onClick={() => (window.location.href = `../editpost/${postId}`)}>수정하기</EditPostButtonField>;
-//     } else {
-//         return <></>;
-//     }
-// };
 
 const ViewPostContentBlock = ({ postTitle, postContent }) => {
     return (
@@ -122,18 +112,10 @@ const ViewPostContentBlock = ({ postTitle, postContent }) => {
     );
 };
 
-const ViewCommentBlock = () => {
-    return (
-        <ViewCommentContainer>
-            <ViewCommentLayout></ViewCommentLayout>
-        </ViewCommentContainer>
-    );
-};
-
 const WriteCommentBlock = ({createDate, writerName}) => {
     const [visible, setVisible] = useState(false);
     const [comment, setComment] = useState('');
-    const [isVaild, setIsVaild] = useState(false);   //유효성 검사
+    const [isVaild, setIsVaild] = useState(false);
     const [feedComments, setFeedComments] = useState([]);
     const [isOpen, setMenu] = useState(false);
     const [showMenu, setShowMenu] = useState()
@@ -150,25 +132,21 @@ const WriteCommentBlock = ({createDate, writerName}) => {
         setVisible(true);
     }
 
-    const linkCopy = () => {
-        var url = '';
-        var textArea = document.createElement("textarea");
-        document.body.appendChild(textArea);
-        url = window.document.location.href;
-        textArea.value = url;
-        textArea.select();
-        navigator.clipboard.writeText(url);
-        document.body.removeChild(textArea);
-        alert("링크가 복사되었습니다.")
-    }
-    const refreshPage = () => {
-        window.location.reload();
-    }
-
     const toggleMenu = () => {
         setMenu(isOpen => !isOpen);
     }
-    
+
+    const onClick = () => {
+        if (comment.length > 0) {
+            post()
+            console.log("댓글이 작성되었습니다.");
+            handleViewComments();
+        }
+        else {
+            alert("댓글을 작성해주세요.")
+        }
+    }
+
 
     return (
         <>  
@@ -177,9 +155,6 @@ const WriteCommentBlock = ({createDate, writerName}) => {
                     return (
                         <ViewCommentContainer visible={visible}>
                             <ViewCommentUserImgLayout src="https://media.istockphoto.com/id/1197796372/ko/%EB%B2%A1%ED%84%B0/%EC%82%AC%EB%9E%8C-%EB%B2%A1%ED%84%B0-%EC%95%84%EC%9D%B4%EC%BD%98%EC%9E%85%EB%8B%88%EB%8B%A4-%EC%82%AC%EB%9E%8C-%EC%95%84%EC%9D%B4%EC%BD%98.jpg?s=612x612&w=0&k=20&c=O4BhlKJtKHevLMEJqMIim3IKseu5lEYXBOm3uI8r_vk="></ViewCommentUserImgLayout>
-                            {/* <ViewPostMenuImg src='/img/dots.png' style={{
-                                width: '2rem', height: '2rem', paddingTop: '0.3rem', float: 'right'
-                                }} onClick={()=>{}}></ViewPostMenuImg> */}
                             <ViewCommentMenuLayout style={{position: 'relative'}} onClick={() => {toggleMenu(); setShowMenu(i);}}>
                                 <p>➕</p>
                                 {
@@ -218,8 +193,6 @@ const WriteCommentBlock = ({createDate, writerName}) => {
             }
 
             <WriteCommentContainer>
-                {/* <ProfileImage src='https://media.istockphoto.com/id/1197796372/ko/%EB%B2%A1%ED%84%B0/%EC%82%AC%EB%9E%8C-%EB%B2%A1%ED%84%B0-%EC%95%84%EC%9D%B4%EC%BD%98%EC%9E%85%EB%8B%88%EB%8B%A4-%EC%82%AC%EB%9E%8C-%EC%95%84%EC%9D%B4%EC%BD%98.jpg?s=612x612&w=0&k=20&c=O4BhlKJtKHevLMEJqMIim3IKseu5lEYXBOm3uI8r_vk='
-                style={{margin: '1rem'}}/> */}
                 <WriteCommentLayout type={'text'} placeholder={"댓글을 입력해주세요"}
                 onChange={(e)=>{
                     setComment(e.target.value);
@@ -230,14 +203,14 @@ const WriteCommentBlock = ({createDate, writerName}) => {
                 }} value={comment}></WriteCommentLayout>
                 <UploadCommentLayout onClick={()=>{
                     if (comment.length > 0) {
-                        post()
+                        post();
                         console.log("댓글이 작성되었습니다.");
                         handleViewComments();
                     }
                     else {
-                        alert("댓글을 작성해주세요.")
+                        alert("댓글을 작성해주세요.");
                     }
-                }} disabled={isVaild ? false : true} >작성</UploadCommentLayout>
+                }} disabled={isVaild ? false : true}>작성</UploadCommentLayout>
             </WriteCommentContainer>
         </>
 
