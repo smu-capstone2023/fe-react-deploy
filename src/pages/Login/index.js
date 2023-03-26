8538d201a47478f035d080ba90d1359518e30b78
Seyounie <aras3640@gmail.com>
2023-03-27 00:19:58 +0900

    [fix] 로그인,회원가입 api 수정 완료

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -10,38 +10,42 @@ import {
 } from "./LoginStyles";
 
 const Login = () => {
-    const [school_id, setSchoolId] = useState(""); // setEmail을 setSchoolId로 변경
-    const [password, setPassword] = useState("");
-    
-    const requestLoginToServer = () => {
-      if (school_id === "" || password === "") {
-        alert("회원정보를 입력해주세요.");
-      } else {
-        axios
-          .post(`${process.env.REACT_APP_SERVER_URL}:8001/auth/login`, {
+  const [school_id, setSchoolId] = useState(""); // setEmail을 setSchoolId로 변경
+  const [password, setPassword] = useState("");
+
+  const requestLoginToServer = () => {
+    if (school_id === "" || password === "") {
+      alert("회원정보를 입력해주세요.");
+    } else {
+      console.log(school_id, "@", password);
+      axios
+        .get(`${process.env.REACT_APP_SERVER_URL}:8001/auth/login`, {
+          headers: {
             school_id: school_id,
             password: password,
-          })
-          .then((response) => {
-            console.log(response);
-            setUserInfoAtLocalStorage(response.data);
-            window.location.href = "/";
-          })
-          .catch((response) => {
-            console.log(response);
-            alert("로그인 정보가 없습니다.");
-          });
-      }
-    };
-  
-    const setUserInfoAtLocalStorage = (response) => {
-      localStorage.setItem("school_id", response.school_id);
-      localStorage.setItem("nickname", response.nickname);
-      localStorage.setItem("majorList", JSON.stringify(response.majorlist));
-      localStorage.setItem("access_token", response.access_token);
-      localStorage.setItem("refresh_token", response.refresh_token);
-    };
+            'Content-type': 'application/json',
+            Accept: 'application/json',
+        },
+        })
+        .then((response) => {
+          console.log(response);
+          setUserInfoAtLocalStorage(response.data);
+          window.location.href = "/";
+        })
+        .catch((response) => {
+          //예외처리
+          //response
+          console.log(school_id, "@", password);
+          console.log(response);
+          alert("로그인 정보가 없습니다.");
+        });
+    }
+  };
 
+  const setUserInfoAtLocalStorage = (response) => {
+    localStorage.setItem("access_token", response.access_token);
+    localStorage.setItem("refresh_token", response.refresh_token);
+  };
 
   return (
     <>
