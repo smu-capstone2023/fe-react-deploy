276f43b670d1bae2adf6e9b46c3655aa7d8cd9b2
sunjungAn <dkstjswjd83@naver.com>
2023-03-30 22:15:03 +0900

    feat: 로그인 유무에 따라 sidebar 뷰 수정

--- src/component/Sidebar/index.js
+++ src/component/Sidebar/index.js
@@ -1,9 +1,9 @@
 import { SidebarContainer, Icon, CloseIcon, SidebarMenu, SidebarWrapper, SidebarLink } from './SidebarStyles';
-const Sidebar = ({ isOpen, toggle, setIsOpen, isLogin }) => {
+const Sidebar = ({ isOpen, toggle, setIsOpen }) => {
     const logout = () => {
         localStorage.clear();
+        window.location.href = '/';
     };
-
     return (
         <>
             <SidebarContainer isOpen={isOpen}>
@@ -12,19 +12,23 @@ const Sidebar = ({ isOpen, toggle, setIsOpen, isLogin }) => {
                 </Icon>
                 <SidebarWrapper>
                     <SidebarMenu>
-                        <SidebarLink to='board/004003' onClick={() => setIsOpen(false)}>
-                            학교게시판
-                        </SidebarLink>
-                        <SidebarLink to='major' onClick={() => setIsOpen(false)}>
-                            학과게시판
-                        </SidebarLink>
-                        <SidebarLink to='feedback' onClick={() => setIsOpen(false)}>
-                            피드백게시판
-                        </SidebarLink>
-                        <SidebarLink to='mypage' onClick={() => setIsOpen(false)}>
-                            마이페이지
-                        </SidebarLink>
-                        {isLogin ? (
+                        {localStorage.getItem('access_token') && (
+                            <>
+                                <SidebarLink to='board/3' onClick={() => setIsOpen(false)}>
+                                    학교게시판
+                                </SidebarLink>
+                                <SidebarLink to='board/2' onClick={() => setIsOpen(false)}>
+                                    학과게시판
+                                </SidebarLink>
+                                <SidebarLink to='feedback' onClick={() => setIsOpen(false)}>
+                                    피드백게시판
+                                </SidebarLink>
+                                <SidebarLink to='mypage' onClick={() => setIsOpen(false)}>
+                                    마이페이지
+                                </SidebarLink>
+                            </>
+                        )}
+                        {localStorage.getItem('access_token') ? (
                             <>
                                 <SidebarLink
                                     onClick={() => {
