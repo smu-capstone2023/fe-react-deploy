0590155d3e625163fcb97bc3c81aad27ae840bb0
hyun <jjjtoy@naver.com>
2023-04-02 19:07:23 +0900

    2차 수정

--- src/component/PostListElement/NoticeLong/index.js
+++ src/component/PostListElement/NoticeLong/index.js
@@ -1,6 +1,6 @@
 import {WriterNameField, NoticeElementLink, NoticeNumberOfComment, NoticeTitle, CreateDateField, NoticeElementLayout, NoticeBasicInfoField, NumberOfViewsField} from './NoticeLongStyles';
 const NoticeLong = ({title, createDate, numberOfComment, postId, numberOfViews, writerName}) => {
-    const limitedTitle = title.slice(0,20) +"..."
+    const limitedTitle =  title.length > 20 ? title.slice(0,20) +"..." : title;
     console.log(title)
     return (
         <>
