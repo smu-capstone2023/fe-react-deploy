bffa95c5177d627cff75bc90910b0aa7f52dd8b6
차다인 <cdin23@naver.com>
2023-04-12 00:19:52 +0900

    feat: #217 SignUpSite

---
+++ src/api/User/join.js
@@ -0,0 +1,26 @@
+import axios from "axios"
+
+const SignUpSite = (shcool_id, nickname, password, image) => {
+    axios
+        .post(`${process.env.REACT_APP_SERVER_URL}/auth/join`, 
+        {
+            school_id: shcool_id,
+            nickname: nickname,
+            password: password,
+            image: image,
+        },
+        {
+            headers: {
+                Authorization: localStorage.getItem('access_token'),
+            },
+        })
+        .then((response) => {
+            if (response.data.status_code === 201) {
+                return response.data.profile_image_url;
+            }
+        })
+        .catch((response) => {
+            console.log(response);
+            return '';
+        });
+};
