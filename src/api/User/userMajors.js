3e88dea44f67cbf9b9a12776c9b0df71ab875a98
차다인 <cdin23@naver.com>
2023-04-12 00:53:40 +0900

    feat: #217 GetUserMajorsInfo

---
+++ src/api/User/userMajors.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+const GetUserMajorsInfo = () => {
+    axios
+        .get(`${process.env.REACT_APP_SERVER_URL}/auth/usermajors`, {
+            headers: {
+                Authorization: localStorage.getItem('access_token'),
+            },
+        })
+        .then((response) => {
+            if (response.data.status_code === 200) {
+                return response.data;
+            }
+        })
+        .catch((response) => {
+            console.log(response);
+            return [];
+        })
+
+}
