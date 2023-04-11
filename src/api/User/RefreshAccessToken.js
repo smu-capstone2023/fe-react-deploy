2aa2797b6f8e96a27ba6ab89ad79e2c643cf79eb
차다인 <cdin23@naver.com>
2023-04-12 00:47:23 +0900

    feat: #217 RefreshAccessTokenIssue

---
+++ src/api/User/RefreshAccessToken.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+const RefreshAccessTokenIssue = () => {
+    axios
+        .get(`${process.env.REACT_APP_SERVER_URL}/auth/refresh_access_token`, {
+            headers : {
+                Authorization: localStorage.getItem('access_token'),
+                Authorization: localStorage.getItem('refresh_token'),
+            },
+        })
+        .then((response) => {
+            if (response.data.status_code === 240) {
+                return response.data.access_token;
+            }
+        })
+        .catch((response) => {
+            console.log(response);
+            return '';
+        })
+};
