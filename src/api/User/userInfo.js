a3f0b600bdd157e3610bc11b5da609b1537363e9
차다인 <cdin23@naver.com>
2023-04-12 18:55:25 +0900

    Feat: #217 getUserInfo

---
+++ src/api/User/userInfo.js
@@ -0,0 +1,17 @@
+const { default: axios } = require("axios")
+
+const getUserInfo = () => {
+    axios
+        .get(`${process.env.REACT_APP_SERVER_URL}/auth/user_info`, {
+            headers: {
+                Authorization: localStorage.getItem('access_token'),
+            },
+        })
+        .then((response) => {
+            return response.data;
+        })
+        .catch((response) => {
+            console.log(response);
+            return {};
+        })
+}
