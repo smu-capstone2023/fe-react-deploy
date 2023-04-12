ae2ffa1846faacaebfaa460e5f1ddb124f20a13c
차다인 <cdin23@naver.com>
2023-04-12 19:08:57 +0900

    Feat: #217 ChangeNickname

---
+++ src/api/User/nickname.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+
+const ChangeNickname = (nickname) => {
+    axios
+        .put(`${process.env.REACT_APP_SERVER_URL}/auth/user/nickname`, 
+        {
+            nickname : nickname,
+        },
+        {
+            headers: {
+                Authorization: localStorage.getItem('access_token'),
+            },
+        })
+        .then((response) => {
+            return response.data.nickname;
+        })
+        .catch((response) => {
+            console.log(response);
+            return '';
+        })
+}
