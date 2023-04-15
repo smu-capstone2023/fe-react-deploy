03ad712fe38fe57e78ef21a1688ee257fbc9c29c
차다인 <cdin23@naver.com>
2023-04-15 16:06:55 +0900

    Feat: #233 getBaordDetailPostList

---
+++ src/api/BoardApi/getBaordDetailPostList.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+export const getBaordDetailPostList = (board_id, sorting) => {
+    return axios
+        .get(`${process.env.REACT_APP_SERVER_URL}/board/post_list/${board_id}`,
+        {
+            headers: {
+                Authorization: localStorage.getItem('access_token'),
+                sorting : sorting,
+            },
+        })
+        .then((response) => {
+            return response.data.posts;
+        })
+        .catch((response) => {
+            console.log(response);
+            return [];
+        })
+};
