467e56c2cd67768e9e7e578a2bbb0511f603e9cf
hyun <jjjtoy@naver.com>
2023-04-30 18:05:26 +0900

    3차수정

--- src/component/molecule/IconButton.jsx
+++ src/component/molecule/IconButton.jsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
+
 export const IconButton = ({icon, title, onClick, fontStyles, iconSize}) => {
     
     return(
         <IconButtonLayout onClick={onClick}>
-            <p style={{fontSize:`${iconSize}`}}>{icon}</p> 
+            <p style={{fontSize:`${iconSize}em`}}>{icon}</p> 
             <p style={fontStyles}>{title}</p> 
         </IconButtonLayout>
     )
@@ -15,7 +16,6 @@ const IconButtonLayout = styled.div`
     display:flex;
     flex-direction: column;
     align-items: center;
-    background-color: red;
     padding: 1em;
-    // font-family: "NEXON Lv1 Gothic Regular: nexon-regular";
+    ;
 `;
