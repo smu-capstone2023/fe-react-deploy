9d1b50ce39922f3cb766c6112b4ea56231e9b758
차다인 <cdin23@naver.com>
2023-05-01 02:36:22 +0900

    feat: #242 LostPreviewElement

---
+++ src/component/molecule/LostPreviewElement.jsx
@@ -0,0 +1,15 @@
+import { 
+    Box,
+    Text,
+} from '@chakra-ui/react'
+
+export const LostPreviewElement = ({onClick, content}) => {
+    return (
+        <>
+            <Box bg='#EBF0FF' w='30%' p={4} borderWidth='1px' borderRadius='lg' color='#3E3E3E' onClick={onClick}>
+                <Text as='b' fontSize='lg' color='#4169E1'>NEW!</Text>
+                <Text fontSize='md'>{content}</Text>
+            </Box>
+        </>
+    )
+};
