7943fdbe5c4f43d3e0271749c16292f5c13cd1fe
hyun <jjjtoy@naver.com>
2023-04-30 16:34:10 +0900

    2차 수정

--- src/component/molecule/IconButton.jsx
+++ src/component/molecule/IconButton.jsx
@@ -1,13 +1,21 @@
-import {IconButtonLayout} from './IconButton'
-
+import styled from 'styled-components';
 
 export const IconButton = ({icon, title, onClick, fontStyles, iconSize}) => {
     
     return(
         <IconButtonLayout onClick={onClick}>
-            <div size={iconSize}>{icon}</div> 
-            <p styles={fontStyles}>{title}</p> 
+            <p style={{fontSize:`${iconSize}`}}>{icon}</p> 
+            <p style={fontStyles}>{title}</p> 
         </IconButtonLayout>
     )
 
 } 
+
+const IconButtonLayout = styled.div`
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: red;
+    padding: 1em;
+    // font-family: "NEXON Lv1 Gothic Regular: nexon-regular";
+`;
