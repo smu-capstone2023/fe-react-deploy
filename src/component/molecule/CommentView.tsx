8dfa11c6209d5f943b554557c27a496b559785af
Seyounie <aras3640@gmail.com>
2023-05-01 11:03:38 +0900

    fix: #204 CommentView

--- src/component/molecule/CommentView.tsx
+++ src/component/molecule/CommentView.tsx
@@ -1,32 +1,15 @@
 import React from 'react';
-import { useState } from 'react';
 import { IoChatboxOutline } from "react-icons/io5";
 
-const CommentView = () => {
-//댓글수 카운트
-  const [commentCount, setCommentCount] = useState(0);
-
-  const handleCommentClick = () => {
-    setCommentCount(prevCount => prevCount + 1);
-  };
-
+//댓글 수
+const CommentView = ({ commentCount }) => {
   return (
-    //컨테이너
-    <div
-      style={{
-        width: 38,
-        height: 14,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-      }}
-    >
-      <IoChatboxOutline
-        size={10}
-        color="#4169E1"
-        onClick={handleCommentClick}
-      />
-      <span style={{ fontSize: 10, color: '#4169E1' }}>{commentCount}</span>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <IoChatboxOutline size={14} color="#4169E1" />
+      <span 
+        style={{ fontSize: 14, color: '#FFFFFF', marginLeft: 4 }}>
+        {commentCount}
+      </span>
     </div>
   );
 };
