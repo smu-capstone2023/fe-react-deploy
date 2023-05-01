bf00ffa26a6368280326a0a4d943902d6ab8398e
Seyounie <aras3640@gmail.com>
2023-05-01 11:03:50 +0900

    fix: #204 LikeView

--- src/component/molecule/LikeView.tsx
+++ src/component/molecule/LikeView.tsx
@@ -1,32 +1,15 @@
 import React from 'react';
-import { useState } from 'react';
 import { AiOutlineLike } from 'react-icons/ai';
 
-const LikeView = () => {
-//좋아요수 카운트
-  const [likeCount, setLikeCount] = useState(0);
-
-  const handleLikeClick = () => {
-    setLikeCount(prevCount => prevCount + 1);
-  };
-
+//좋아요 수
+const LikeView = ({ likeCount }) => {
   return (
-    //컨테이너
-    <div
-      style={{
-        width: 38,
-        height: 14,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-      }}
-    >
-      <AiOutlineLike
-        size={10}
-        color="#FF5A5A"
-        onClick={handleLikeClick}
-      />
-      <span style={{ fontSize: 10, color: '#FF5A5A' }}>{likeCount}</span>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <AiOutlineLike size={14} color="#FF5A5A" />
+      <span 
+        style={{ fontSize: 10, color: '	#FFFFFF', marginLeft: 4 }}>
+        {likeCount}
+      </span>
     </div>
   );
 };
