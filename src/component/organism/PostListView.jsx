8aae72a3023020d93d85758e1caf06cb5c5deaec
hyun <jjjtoy@naver.com>
2023-05-07 00:02:31 +0900

    feat: #269 임시 데이터 제거

--- src/component/organism/PostListView.jsx
+++ src/component/organism/PostListView.jsx
@@ -6,61 +6,11 @@ import PostListElement from "../molecule/PostListElement";
  * @returns
  */
 
-const PostListView = () => {
-    const postListData = [
-        {
-          onClick: () => alert("Post 1 clicked!"),
-          commentCount: 10,
-          likeCount: 5,
-          title: "Post 1",
-          content: "This is the first post.",
-          createdate: "2022-05-10",
-        },
-        {
-          onClick: () => alert("Post 2 clicked!"),
-          commentCount: 3,
-          likeCount: 8,
-          title: "Post 2",
-          content: "This is the second post.",
-          createdate: "2022-05-11",
-        },
-        {
-          onClick: () => alert("Post 3 clicked!"),
-          commentCount: 7,
-          likeCount: 2,
-          title: "Post 3",
-          content: "This is the third post.",
-          createdate: "2022-05-12",
-        },
-        {
-          onClick: () => alert("Post 4 clicked!"),
-          commentCount: 2,
-          likeCount: 6,
-          title: "Post 4",
-          content: "This is the fourth post.",
-          createdate: "2022-05-13",
-        },
-        {
-          onClick: () => alert("Post 5 clicked!"),
-          commentCount: 5,
-          likeCount: 3,
-          title: "Post 5",
-          content: "This is the fifth post.",
-          createdate: "2022-05-14",
-        },
-        {
-          onClick: () => alert("Post 6 clicked!"),
-          commentCount: 9,
-          likeCount: 1,
-          title: "Post 6",
-          content: "This is the sixth post.",
-          createdate: "2022-05-15",
-        },
-      ];
-      
+const PostListView = ({postListData}) => {
+    
     return(
         <div style={{width: '100%'}}>
-            {postListData.map((postData, index) => (
+            {postListData && postListData.map((postData, index) => (
             <PostListElement
                 key={index}
                 onClick={postData.onClick}
