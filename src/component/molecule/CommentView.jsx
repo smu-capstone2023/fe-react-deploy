9253a3472ce70632beef99e6ec32b1e6393d1626
sunjungAn <dkstjswjd83@naver.com>
2023-05-06 22:37:52 +0900

    feat: fontSize, iconSize 입력값 받도록 설정

--- src/component/molecule/CommentView.jsx
+++ src/component/molecule/CommentView.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 import { IoChatboxOutline } from "react-icons/io5";
 
-//댓글 수
-export const CommentView = ({ commentCount }) => {
-  return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <IoChatboxOutline size={11} color="#4169E1" />
-      <span 
-        style={{ fontSize: 12,color:'#4169E1', marginLeft: 4 }}>
-        {commentCount}
-      </span>
-    </div>
-  );
+/**
+ *
+ * @param commentCount: number
+ * @param fontSize: number
+ * @param iconSize: number
+ * @returns
+ */
+export const CommentView = ({ commentCount, fontSize = 12, iconSize = 12 }) => {
+    return (
+        <div style={{ display: "flex", alignItems: "center" }}>
+            <IoChatboxOutline size={iconSize} color="#4169E1" />
+            <span style={{ fontSize: fontSize, color: "#4169E1", marginLeft: 4 }}>{commentCount}</span>
+        </div>
+    );
 };
 
 export default CommentView;
