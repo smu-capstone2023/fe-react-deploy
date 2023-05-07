65bb248106329d5c3c3362c0fd5af064bd046f66
hyun <jjjtoy@naver.com>
2023-05-07 22:32:19 +0900

    feat: #285 BoardSelectBox 컴포넌트 제작(미완성)

---
+++ src/component/molecule/BoardSelectBox.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+/**
+ * @param boardList : {id: number, name: string}[]
+ * @param onClick: () => {} 
+ * @returns
+ */
+
+const BoardSelectBox = () => {
+    // const boardList = [{id:0, name: '자유게시판'},{id:1, name: '비밀게시판'},{id:2, name: '공지게시판'}];
+    // const onClick = () => alert('ㅋ')
+    return(
+    <BoardSelectBoxLayout>
+        z
+    </BoardSelectBoxLayout>
+    )
+}
+
+const BoardSelectBoxLayout =styled.div`
+    width: 100em;
+    background: red;
+`
+
+export default BoardSelectBox;
+
+
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import HomeView from "../component/template/HomeView";
-
+import BoardSelectBox from "../component/molecule/BoardSelectBox";
 const Home = () => {
     return (
+        <>
+        <BoardSelectBox></BoardSelectBox>
         <HomeView
             userInfo={{ nickname: "키야", schoolId: "1234r5", major: "컴퓨터과학과", mbti: "INTJ" }}
             hotPreviewList={[
@@ -15,6 +17,7 @@ const Home = () => {
             majorInfo={{ majorName: "컴퓨터과학과", majorId: 12 }}
             lostPreviewList={[{ content: "분실물 데이터입니다." }, { content: "분실물 데이터입니다." }]}
         ></HomeView>
+         </>
     );
 };
 
