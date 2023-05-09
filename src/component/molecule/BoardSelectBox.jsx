806478abeab0ed193f14bedc7b974038bdcfa66d
hyun <jjjtoy@naver.com>
2023-05-10 00:31:16 +0900

    feat: #289 파라미터 세팅 및 css 수정

--- src/component/molecule/BoardSelectBox.jsx
+++ src/component/molecule/BoardSelectBox.jsx
@@ -7,48 +7,30 @@ import styled from "styled-components";
  * @returns
  */
 
-const BoardSelectBox = () => {
-    const boardList = [{id:0, name: '자유게시판'},{id:1, name: '비밀게시판'},{id:2, name: '공지게시판'},{id:3, name: '홍보게시판'}];
+const  BoardSelectBox = ({boardList, onClick}) => {
     return(
     <BoardSelectBoxLayout>
-        {boardList && boardList.map(item => (<BoardSelectdiv key={item.id} onClick={()=>alert(item.id)}>{item.name}</BoardSelectdiv>))}
+        {boardList && boardList.map(item => (<BoardSelectdiv key={item.id} onClick={()=>onClick}>{item.name}</BoardSelectdiv>))}
     </BoardSelectBoxLayout>
     )
 }
 
-const BoardSelectBoxLayout =styled.div`
+const BoardSelectBoxLayout = styled.div`
     display: flex;
     width: 100%;
-    justify-content: start;
-    border-bottom: 0.3px solid #B6B6B6;
-`
+    border-bottom: 0.3px solid #b6b6b6;
+    gap: 1rem;
+`;
 
-const BoardSelectdiv =styled.div`
+const BoardSelectdiv = styled.div`
     display: flex;
-    justify-content: center;
-    width: 13%;
-    padding-bottom: .6rem;
+    padding-bottom: 0.6rem;
     font-family: nexon-regular;
 
-    @media screen and (max-width: 1200px) {
-        width: 16%;
-        font-size: .9em;    
-     }
-
-    @media screen and (max-width: 880px) {           
-        width: 90%;
-        padding-bottom: .8rem;
-        font-size: .7em;
-        justify-content: center;
+    @media screen and (max-width: 600px) {
+        font-size: 0.8em;
     }
-
-    @media screen and (max-width: 400px) {           
-        width: 90%;
-        padding-bottom: .7rem;
-        font-size: .6em;
-        justify-content: center;
-    }
-`
+`;
 
 export default BoardSelectBox;
 
