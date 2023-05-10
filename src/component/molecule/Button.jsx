135b664ec6df237a459d1c5ec187e897764b6940
sunjungAn <dkstjswjd83@naver.com>
2023-05-10 15:43:49 +0900

    fix: #299 button width 인자 추가

--- src/component/molecule/Button.jsx
+++ src/component/molecule/Button.jsx
@@ -1,28 +1,32 @@
 /**
  * @param title: {string}
  * @param onClick: () => {}
+ * @param width: number | string
  * @returns
  */
 
-export const Button = ({title, onClick}) => {
+export const Button = ({ title, onClick, width = "100%" }) => {
     return (
         <>
-        <div style={{ display: "flex", width: "100%" }} onClick={onClick}>
-                <button style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: '#FFFFFF',
-                    background: '#4169E1',
-                    height: '3rem',
-                    width: '100%',
-                    padding: '1rem 2rem 1rem 2rem',
-                    borderRadius: '6px',
-                }}>{title}</button>
+            <div style={{ display: "flex", width: width }} onClick={onClick}>
+                <button
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "#FFFFFF",
+                        background: "#4169E1",
+                        height: "3rem",
+                        width: "100%",
+                        padding: "1rem 2rem 1rem 2rem",
+                        borderRadius: "6px",
+                    }}
+                >
+                    {title}
+                </button>
             </div>
         </>
-    )
+    );
 };
 
 export default Button;
-
