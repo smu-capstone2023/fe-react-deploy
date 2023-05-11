fe55c634a7842f05e097a206d31d0d718972526d
hyun <jjjtoy@naver.com>
2023-05-11 16:50:23 +0900

    feat #300 Page signup 로직 수정

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,24 +6,25 @@ const Signup = () => {
     const [id, setId] = useState("");
     const [password, setPassword] = useState("");
     const [rePassword, setRePassword] = useState("");
-
+ 
+    //임시 값 부여
     const nickname = 'z'
 
     const checkIdRegExp = () => {
         //Ex) 123456789(9자리 숫자)
         return /^\d{9}/.test(id);
-        
-    }
+    };
+
     const checkPasswordExp = () => {
         //Ex) qwer1234! (숫자,문자 필수, 특수문자 허용, 8~16자리)
-        return(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+]{8,16}$/.test(password))
+        return (/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+]{8,16}$/.test(password));
     };
 
     const checkRePasswordExp = () => {
         if (password === rePassword) {
-            return(true)
+            return true;
         } else {
-            return(false)
+            return false;
         }
     };
 
@@ -34,17 +35,15 @@ const Signup = () => {
         else {
             signUpSite(id, password, nickname,)
                 .then((response)=>{
-                    if (response.status === 201) { window.location.href ='/user-certification' }})
-                    
-                // **400 에러가 발생해도 else부분이 작동함**
-                .catch((error) => {
-                    if (error.response.status === 400) { alert('이미 가입하신 회원이십니다.') }
-                    else { alert('네트워크 문제! 잠시후에 다시 시도해주세요! ') }
-                      })
-                    
+                    if(response){
+                        { window.location.href ='/user-certification' }
+                       }
+                    else{
+                       alert('네트워크 문제! 잠시후에 다시 시도해주세요! ');
+                       }
+                    })
             }
         }
-        
 
     return (
         <SignUpView
