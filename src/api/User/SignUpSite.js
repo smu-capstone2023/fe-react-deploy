4e271cf46c25da06e2941a4994e4e4428d7159c5
hyun <jjjtoy@naver.com>
2023-05-11 15:01:38 +0900

    feat #300 API 문서에 없는 image 파라미터 삭제

--- src/api/User/SignUpSite.js
+++ src/api/User/SignUpSite.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-export const signUpSite = (shcool_id, nickname, password, image) => {
+export const signUpSite = (shcool_id, nickname, password) => {
     return axios
         .post(`${process.env.REACT_APP_SERVER_URL}/auth/join`, {
             school_id: shcool_id,
             nickname: nickname,
             password: password,
-            image: image,
         })
         .then((response) => {
             if (response.data.status_code === 201) {
