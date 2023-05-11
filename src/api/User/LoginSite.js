8d9a70296c94b98c59f83bdc214ad24037b22cd7
sunjungAn <dkstjswjd83@naver.com>
2023-05-11 21:19:34 +0900

    feat: LoginSite 함수 수정

--- src/api/User/LoginSite.js
+++ src/api/User/LoginSite.js
@@ -1,19 +1,18 @@
-import axios from "axios"
+import axios from "axios";
 
 export const loginSite = (school_id, password) => {
     return axios
-        .post(`${process.env.REACT_APP_SERVER_URL}/auth/login`, 
-        {
-            school_id : school_id,
-            password : password,
+        .post(`${process.env.REACT_APP_SERVER_URL}/auth/login`, {
+            school_id: school_id,
+            password: password,
         })
         .then((response) => {
             if (response.data.status_code === 200) {
-                return response.data;
+                return { access_token: response.data.access_token, refresh_token: response.data.refresh_token };
             }
         })
         .catch((response) => {
             console.log(response);
             return {};
-        })
-}
+        });
+};
