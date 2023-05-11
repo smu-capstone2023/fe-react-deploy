f36529a7c378dcb11468fe3c3e7651a3281f4e77
hyun <jjjtoy@naver.com>
2023-05-11 15:02:08 +0900

    feat #300 Signup page 로직 작성

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import SignUpView from "../component/template/SignUpView";
+import { signUpSite } from "../api/User/SignUpSite";
 
 const Signup = () => {
     const [id, setId] = useState("");
     const [password, setPassword] = useState("");
     const [rePassword, setRePassword] = useState("");
 
+    const nickname = 'z'
+
     const checkIdRegExp = () => {
         //Ex) 123456789(9자리 숫자)
-        // console.log(id)
         return /^\d{9}/.test(id);
         
     }
@@ -26,11 +28,23 @@ const Signup = () => {
     };
 
     const onClickSignupButton = () => {
-        if (checkIdRegExp() == false) {console.log('ID 형식을 확인해주세요.')} 
+        if (checkIdRegExp() == false) {alert('ID 형식을 확인해주세요.')} 
         else if (checkPasswordExp() == false) {alert('비밀번호 형식을 확인해주세요.')} 
         else if (checkRePasswordExp() == false) {alert('비밀번호가 일치하지 않습니다.')} 
-
-    };
+        else {
+            signUpSite(id, password, nickname,)
+                .then((response)=>{
+                    if (response.status === 201) { window.location.href ='/user-certification' }})
+                    
+                // **400 에러가 발생해도 else부분이 작동함**
+                .catch((error) => {
+                    if (error.response.status === 400) { alert('이미 가입하신 회원이십니다.') }
+                    else { alert('네트워크 문제! 잠시후에 다시 시도해주세요! ') }
+                      })
+                    
+            }
+        }
+        
 
     return (
         <SignUpView
