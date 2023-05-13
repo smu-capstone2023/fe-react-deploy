591825745792b1bea645cecc41e8d2c7586fcd09
sunjungAn <dkstjswjd83@naver.com>
2023-05-14 00:18:23 +0900

    feat: userInfoPostReader 컴포넌트 수정

--- src/component/molecule/UserInfoPostReader.jsx
+++ src/component/molecule/UserInfoPostReader.jsx
@@ -4,7 +4,6 @@ import { IoChatboxOutline } from "react-icons/io5";
 import { BiUserCircle } from "react-icons/bi";
 import styled from "styled-components";
 
-
 /**
  * @param userName: string
  * @param postId: number
@@ -14,38 +13,35 @@ import styled from "styled-components";
  * @returns
  */
 
-const UserInfoPostReader = ({ userName, iconSize = 17, fontSize = '1rem', postId, commentId }) => {
+const UserInfoPostReader = ({ userName, iconSize = 17, fontSize = "1rem", postId, commentId }) => {
+    const handleLikeClick = () => {
+        alert(`좋아요 눌림 ${postId}`);
+    };
 
-  const handleLikeClick = () => {
-    alert(`좋아요 눌림 ${postId}`);
+    const handleCommentClick = () => {
+        alert(`댓글달기 눌림 ${postId}`);
     };
-  
-  const handleCommentClick = () => {
-    alert(`댓글달기 눌림 ${postId}`);
-    };
-    
+
     return (
-      <UserInfoPostReaderLayout style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <div style={{ display: 'flex', alignItems: 'center', fontFamily: 'nexon-regular' }}>
-          <BiUserCircle size={iconSize} style={{ marginRight: '8px', color: '#747474' }} />
-          <div style={{ fontSize }}>{userName}</div>
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <AiOutlineLike size={iconSize} style={{ marginRight: '8px', color: '#4169E1' }} onClick={handleLikeClick} />
-          <IoChatboxOutline size={iconSize} style={{ color: '#4169E1' }} onClick={handleCommentClick} />
-        </div>
-      </UserInfoPostReaderLayout>
+        <UserInfoPostReaderLayout style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <BiUserCircle size={iconSize} style={{ marginRight: "8px", color: "#747474" }} />
+                <p style={{ fontSize, fontFamily: "nexon-regular" }}>{userName}</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <AiOutlineLike size={iconSize} style={{ marginRight: "8px", color: "#4169E1" }} onClick={handleLikeClick} />
+                <IoChatboxOutline size={iconSize} style={{ color: "#4169E1" }} onClick={handleCommentClick} />
+            </div>
+        </UserInfoPostReaderLayout>
     );
-  };
-  
-  const UserInfoPostReaderLayout = styled.div`
-    background: #FFFFFF;
-    padding: 1rem;
-    border: 1px solid #FFFFFF;
+};
+
+const UserInfoPostReaderLayout = styled.div`
+    background: #ffffff;
+    border: 1px solid #ffffff;
     width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    font-family: 'nexon-regular';
-  `;
-  export default UserInfoPostReader;
+`;
+export default UserInfoPostReader;
--- src/component/molecule/UserInfoPostWriter.jsx
+++ src/component/molecule/UserInfoPostWriter.jsx
@@ -5,7 +5,6 @@ import { BsPencilFill } from "react-icons/bs";
 import { BiTrash } from "react-icons/bi";
 import { BiUserCircle } from "react-icons/bi";
 
-
 /**
  * @param userName: string
  * @param postId: number
@@ -15,40 +14,35 @@ import { BiUserCircle } from "react-icons/bi";
  * @returns
  */
 
-const UserInfoPostWriter = ({ userName, iconSize = 17, fontSize = '1rem', postId, commentId }) => {
-
+const UserInfoPostWriter = ({ userName, iconSize = 17, fontSize = "1em", postId, commentId }) => {
     const handleEditClick = () => {
         //alert 띄우기
         alert(`게시글 수정 ${postId}`);
-      };
-      
-      const handleDeleteClick = () => {
+    };
+
+    const handleDeleteClick = () => {
         alert(`게시글 삭제 ${postId}`);
-      };
-    
-      return (
-        <UserInfoPostWriterLayout style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', }}>
-          <div style={{ display: 'flex', alignItems: 'center', fontFamily: 'nexon-regular' }}>
-            <BiUserCircle size={iconSize} style={{ marginRight: '8px', color: '#747474' }} />
-            <div style={{ fontSize, fontFamily: 'nexon-regular' }}>{userName}</div>
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <BsPencilFill size={iconSize} style={{ marginRight: '8px', color: '#4169E1' }} onClick={handleEditClick} />
-            <BiTrash size={iconSize} style={{ color: '#FF5A5A' }} onClick={handleDeleteClick} />
-          </div>
+    };
+
+    return (
+        <UserInfoPostWriterLayout style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <BiUserCircle size={iconSize} style={{ marginRight: "8px", color: "#747474" }} />
+                <p style={{ fontSize: fontSize, fontFamily: "nexon-regular" }}>{userName}</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <BsPencilFill size={iconSize} style={{ marginRight: "8px", color: "#4169E1" }} onClick={handleEditClick} />
+                <BiTrash size={iconSize} style={{ color: "#FF5A5A" }} onClick={handleDeleteClick} />
+            </div>
         </UserInfoPostWriterLayout>
-      );
-  };
-  
-  const UserInfoPostWriterLayout = styled.div`
-    background: #FFFFFF;
-    padding: 1rem;
-    border: 1px solid #FFFFFF;
+    );
+};
+
+const UserInfoPostWriterLayout = styled.div`
+    background: #ffffff;
     width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
-
-    font-family: 'nexon-regular';
-  `;
-  export default UserInfoPostWriter;
+`;
+export default UserInfoPostWriter;
