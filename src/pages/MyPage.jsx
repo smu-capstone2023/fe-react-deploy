8577800e30393333fca632b04ea322f7277443a5
sunjungAn <dkstjswjd83@naver.com>
2023-05-14 23:49:12 +0900

    feat: Mypage.jsx 작업 템플릿 생성

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,13 +1,15 @@
 import MypageView from "../component/template/MypageView ";
 
-const Mypage = () =>{
+const Mypage = () => {
+    const onClickRevoke = () => {
+        // api/User/revoke 함수를 이용해서 true를 반환하면, localStorage를 비워주고 home화면으로 이동시킵니다.
+        // 만약 false라면 네트워크 오류와 같은 에러 메세지를 alert로 띄워주세요.
+    };
+    const onClickLogout = () => {
+        // localStorage를 비워준 뒤에, home화면으로 이동시켜줍니다.
+    };
 
-    const onClickRevoke = () => {alert('준비중(탈퇴하기)')}
-    const onClickLogout = () => {alert('준비중(로그아웃)')}
+    return <MypageView onClickRevoke={onClickRevoke} onClickLogout={onClickLogout} />;
+};
 
-
-    return(
-    <MypageView onClickRevoke={onClickRevoke} onClickLogout={onClickLogout} />
-    )}
-    
-    export default Mypage;
+export default Mypage;
