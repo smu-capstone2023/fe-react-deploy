407dcc953c029982a29b6928fc8b535836c31e97
hyun <jjjtoy@naver.com>
2023-05-15 02:32:17 +0900

    feat: #322MajorCertificateView, MajorCertificate생성

--- src/component/template/MajorCertificateView.jsx
+++ src/component/template/MajorCertificateView.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import Footer from "../organism/Footer"
+import ImageUploadButton from "../molecule/ImageUploadButton";
+import InputTextAreaInput from "../molecule/InputTextAreaBox";
+import Button from "../molecule/Button";
 
 /**
  * @param onClickImageUploadButton: () => {}
@@ -10,46 +13,68 @@ import Footer from "../organism/Footer"
  * @returns
  */
 
-const MajorCertificateView = () => {
+const MajorCertificateView = ({ onClickImageUploadButton, onClickDeleteImageButton, onChangeContent, onClickCompleteButton }) => {
 
     return(
         <>
             <MCVLayout>
-                <div style={{display: 'flex', flex: 2 ,justifyContent:'center', fontFamily:'nexon-regular' }}>학과 인증 절차</div>
 
-                <div style={{flex: 5 }}>
-                    <p style={{fontFamily:'nexon-regular'}}>학과를 인증할 수 있는 이미지를 첨부해주세요.</p>
-                    <p style={{fontFamily:'nexon-regular'}}>사진을 기준으로 학과가 추가됩니다.</p>
-                    <p style={{fontFamily:'nexon-regular'}}>(사진엔 학번도 들어가있어야 합니다!)</p>
-                </div>
+                    <div style={{display: 'flex', flex: 1 ,justifyContent:'center', fontFamily:'nexon-regular' }}>학과 인증 절차</div>
 
-                <div style={{ flex: 5 }}>사진</div>
+                    <div style={{flex: 1 }}>
+                        <p style={{fontFamily:'nexon-regular', fontSize:'.9em'}}>학과를 인증할 수 있는 이미지를 첨부해주세요.</p>
+                        <p style={{fontFamily:'nexon-regular', color: '#747474', fontSize:'.7em'}}>사진을 기준으로 학과가 추가됩니다.</p>
+                        <p style={{fontFamily:'nexon-regular', color: '#4169E1', fontSize:'.7em'}}>(사진엔 학번도 들어가있어야 합니다!)</p>
+                    </div>
 
-                <div style={{flex: 1}}>
-                    <p style={{fontFamily:'nexon-regular'}}>인증 절차에 필요한 기타 말씀해주실 것을 적어주세요!</p>
-                    <p style={{fontFamily:'nexon-regular'}}>(선택)</p>
-                </div>
-            
-                <div style={{flex: 1}}>입력란</div>
+                <EmptyBox/>
 
-                <div style={{flex: 1, fontFamily:'nexon-regular'}}>학과 인증 요청하기</div>
+                    <div style={{flex: 5, width: '100%' }}>
+                        <ImageUploadButton iconSize={'1.5rem'} size={'100%'} onClick={onClickImageUploadButton}></ImageUploadButton>
+                    </div>
+
+                <EmptyBox/>
+
+                    <div style={{flex: 1}}>
+                        <p style={{fontFamily:'nexon-regular', fontSize:'.9em'}}>인증 절차에 필요한 기타 말씀해주실 것을 적어주세요!</p>
+                        <p style={{fontFamily:'nexon-regular', color: '#747474', fontSize:'.7em'}}>(선택)</p>
+                    </div>
+
+                <EmptyBox/>
+
+                    <div style={{flex: 3}}>
+                        <InputTextAreaInput placeholder={'내용을 입력해주세요.'} onChange={onChangeContent} height={"100%"} />
+                    </div>
+
+                <EmptyBox/>
+
+                    <div style={{flex: 1, fontFamily:'nexon-regular', display: 'flex', alignItems: 'center', justifyContent: 'center', }} >
+                        <Button title={'학과 인증 요청하기'} onClick={onClickCompleteButton} width = {"100%"} height = {"2rem"} fontSize = {"1em"} />
+                        </div>
+
             </MCVLayout>
-            <Footer></Footer>
+
+            <div style={{ display: 'flex', flexDirection:'column'}}><Footer/></div>
         </>
         )
 }
 
 const MCVLayout =  styled.div`
     display: flex;
-    justify-content: center;
     flex-direction: column;
-    width: 50%;
-    align-items: center;
+    width: 40%;
     margin: 0 auto;
-    background: red;
-    height: 100%;
+    padding-bottom: 1rem;
+    height: 30rem;
+
+    @media (max-width: 768px) {
+        width: 70%;
+        font-size: .8em;
+      }
+
 `
-
-
+const EmptyBox = styled.div`
+    flex: .5;
+`
 
 export default MajorCertificateView;
--- src/pages/MajorCertificate.jsx
+++ src/pages/MajorCertificate.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import MajorCertificateView from "../component/template/MajorCertificateView";
 
 const MajorCertificate = () => {
-    return <MajorCertificateView />;
+    const onClickImageUploadButton = () => {alert('준비중입니다.(onClickImageUploadButton)')}
+    const onChangeContent = () => {alert('준비중입니다.(onChangeContent)')}
+    const onClickCompleteButton = () => {alert('준비중입니다.(onClickCompleteButton)')}
+    //피그마 미구현
+    const onClickDeleteImageButton = () => {alert('준비중입니다.(onClickDeleteImageButton)')}
+    
+    return(
+            <MajorCertificateView 
+                onClickImageUploadButton={onClickImageUploadButton}
+                onClickDeleteImageButton={onClickDeleteImageButton}
+                onChangeContent={onChangeContent}
+                onClickCompleteButton={onClickCompleteButton}
+            />
+        );
 };
 
 export default MajorCertificate;
