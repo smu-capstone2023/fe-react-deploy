7e04920871b0c1c2354970d3ba6da7ec027b85cc
sunjungAn <dkstjswjd83@naver.com>
2023-05-15 12:31:35 +0900

    feat: commentList

--- src/component/organism/CommentList.jsx
+++ src/component/organism/CommentList.jsx
@@ -8,30 +8,24 @@ import CommentContentView from "./CommentContentView";
  */
 
 const CommentList = ({ commentList }) => {
-
-  return (
-    <div>
-      {commentList.map((comment, index) => (
-        <CommentWrapper>
-        <CommentContentView
-          key={index}
-          comment={comment.comment}
-          author={comment.author}
-          isAuthor={comment.isAuthor}
-        />
-        </CommentWrapper>
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            {commentList &&
+                commentList.map((comment, index) => (
+                    <CommentWrapper key={index}>
+                        <CommentContentView key={index} comment={comment.comment} author={comment.author} isAuthor={comment.isAuthor} />
+                    </CommentWrapper>
+                ))}
+        </div>
+    );
 };
 
 const CommentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  border-bottom: 1px solid #ddd;
-  padding: 15px;
-
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    border-bottom: 1px solid #ddd;
+    padding: 15px;
 `;
 
 export default CommentList;
