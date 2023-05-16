7d23d3456ad436f0ce8bc022b8c2344679e541c6
sunjungAn <dkstjswjd83@naver.com>
2023-05-16 23:40:00 +0900

    feat: getHotBoardPreivewList 함수 완료

--- src/api/board/getHotBoardPreviewList.js
+++ src/api/board/getHotBoardPreviewList.js
@@ -1,10 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
+/**
+ *
+ * @returns postList: {post_id, title, comments, created_time}[]
+ */
 export const getHotBoardPreviewList = () => {
     return axios
         .get(`${process.env.REACT_APP_SERVER_URL}/board/hot`)
-        
         .then((response) => {
-            return response.data; 
+            return response.data;
         })
-
+        .catch((response) => {
+            console.error("getHotBoardPreviewList", response);
+            return [];
+        });
 };
