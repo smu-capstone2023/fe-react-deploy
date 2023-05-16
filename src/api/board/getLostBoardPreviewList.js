03b42f48de9e310e645b58d6f5b8830225894700
sunjungAn <dkstjswjd83@naver.com>
2023-05-17 00:18:01 +0900

    feat: getBoardPreviewList

--- src/api/board/getLostBoardPreviewList.js
+++ src/api/board/getLostBoardPreviewList.js
@@ -1,10 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
+/**
+ *
+ * @returns postList: {post_id, title, comments, created_time}[]
+ */
 export const getLostBoardPreviewList = () => {
     return axios
         .get(`${process.env.REACT_APP_SERVER_URL}/board/lost`)
-        
         .then((response) => {
-            return response.data; 
+            return response.data;
         })
-
+        .catch((response) => {
+            console.error("getLostBoardPreviewList", response);
+            return [];
+        });
 };
