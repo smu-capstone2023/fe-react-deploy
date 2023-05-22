b205b7b9a9139dcfbef12483438e9992955b133d
sunjungAn <dkstjswjd83@naver.com>
2023-05-22 16:46:18 +0900

    Feat: api-addComment 템플릿 작성

---
+++ src/api/Comment/addComment.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+/**
+ * @param post_id: number
+ * @param content: string
+ * @return boolean
+ */
+export const addComment = ({ post_id, content }) => {
+    /**
+     * https://documenter.getpostman.com/view/24666166/2s93JzLLNX#be6eecf4-06cb-4fe9-addb-5ed09a5786c6
+     * 위의 포스트맨을 참고하여 로직을 작성해주세요.
+     * 만약, 댓글 작성이 실패하면, false를, 성공하면 true를 반환해주세요.
+     */
+};
