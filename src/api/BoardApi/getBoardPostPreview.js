06004dd339a43c41256b21642017fcda45977b1c
sunjungAn <dkstjswjd83@naver.com>
2023-05-28 16:51:32 +0900

    feat: getBoardPostPreview api 수정

--- src/api/BoardApi/getBoardPostPreview.js
+++ src/api/BoardApi/getBoardPostPreview.js
@@ -1,17 +1,17 @@
-import axios from "axios"
+import axios from "axios";
 
 export const getBoardPostPreview = (board_id, limit_post_num) => {
     return axios
         .get(`${process.env.REACT_APP_SERVER_URL}/board/preview?board_id=${board_id}&limit_post_num=${limit_post_num}`, {
             headers: {
-                Authorization: localStorage.getItem('access_token'),
+                Authorization: localStorage.getItem("access_token"),
             },
         })
         .then((response) => {
             return response.data;
         })
         .catch((response) => {
-            console.log(response);
-            return {};
-        })
-}
+            console.error("getBoardPostPreview", response);
+            return [];
+        });
+};
