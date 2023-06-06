21d18346ab04f31ff908c4ab16b8c2379364af4b
sunjungAn <dkstjswjd83@naver.com>
2023-06-07 02:18:54 +0900

    feat: reload

--- src/component/organism/LostPreview.jsx
+++ src/component/organism/LostPreview.jsx
@@ -42,7 +42,7 @@ export const LostPreview = ({ previewList }) => {
                                     key={i}
                                     onClick={() => {
                                         localStorage.getItem("school_id")
-                                            ? (window.location.href = `/viewpost/${process.env.REACT_APP_LOST_BOARD_ID}/${data.post_id}`)
+                                            ? (window.location.href = `/viewpost/18/${data.post_id}`)
                                             : alert("로그인이 필요한 기능입니다.");
                                     }}
                                     content={data.title}
