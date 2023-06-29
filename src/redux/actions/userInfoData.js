85a057d8211dd71441541795025f35fdeb0b8e8e
차다인 <cdin23@naver.com>
2023-06-30 01:56:29 +0900

    research: #386 초기값 수정

--- src/redux/actions/userInfoData.js
+++ src/redux/actions/userInfoData.js
@@ -1,16 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getUserInfo } from '../../api/User/getUserInfo';
+import { initialState } from '../slices/userSlice';
 
 const userInfoData = createAsyncThunk(
     'userSlice/userInfoData',
     async() => {
 		try {
-			const response = await getUserInfo();
-			const data = response || [];
-			return data;
+			if (localStorage.getItem("access_token")) {
+				const response = await getUserInfo();
+				const data = response || {};
+				return data;
+			} else { return initialState; }
 		} catch (error) {
 			console.error(error);
-			return '';
+			return {};
     	}
 	}
 );
--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,11 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 import userInfoData from '../actions/userInfoData';
 
+export const initialState = {
+	username: "",
+	school_id: "",
+	profile_img_url: null,
+	majors: [
+	  {
+		major_id: 1,
+		major_name: "상명대학교",
+		free_board_id: 1
+	  }
+	]
+};
+
 const userSlice = createSlice({
 	name: 'user',
-	initialState : {
-        username: "",
-    },
+	initialState : {initialState},
 	reducers : {},
 	extraReducers : (builder) => {
 		builder.addCase(userInfoData.pending, (state,action)=>{
@@ -13,11 +24,10 @@ const userSlice = createSlice({
 		})
 		builder.addCase(userInfoData.fulfilled, (state,action)=>{
 			const response = action.payload;
-            console.log(response);
             return response;
 		})
 		builder.addCase(userInfoData.rejected, (state,action)=>{
-			return '';
+			return state;
 		})
 	},
 });
