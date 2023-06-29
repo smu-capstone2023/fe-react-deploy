05a514c3f770892f71cab773e9b28f37c8daa9c9
sunjungAn <dkstjswjd83@naver.com>
2023-06-29 17:11:41 +0900

    feat: #387 lost-preview font-size 고정

--- src/component/molecule/SectionListHeader.jsx
+++ src/component/molecule/SectionListHeader.jsx
@@ -1,27 +1,23 @@
 import { IoChevronForwardOutline } from "react-icons/io5";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const SectionListHeader = ({ onClick, title }) => {
-    
-  return (
-    <SectionListHeaderLayout onClick={onClick} style={{ display: "flex", alignItems: "center" }}>
-      <h2 style={{ marginRight: "10px", fontFamily: 'nexon-regular' }}>{title}</h2>
-      <IoChevronForwardOutline size={22} color="#888" />
-    </SectionListHeaderLayout>
-  );
+    return (
+        <SectionListHeaderLayout onClick={onClick} style={{ display: "flex", alignItems: "center" }}>
+            <p style={{ marginRight: "10px", fontFamily: "nexon-regular", fontSize: "1rem" }}>{title}</p>
+            <IoChevronForwardOutline size={22} color="#888" />
+        </SectionListHeaderLayout>
+    );
 };
 
 const SectionListHeaderLayout = styled.div`
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 0.3px solid #5E5E5E;
-  align-items: center;
-  padding: 1em;
-  width: 100%;
-  height: 48px;
-  cursor: pointer;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 0.3px solid #5e5e5e;
+    align-items: center;
+    padding: 0.8rem 1rem;
+    width: 100%;
+    cursor: pointer;
 `;
-
-
 
 export default SectionListHeader;
