7c4a4e159f840e83620d5231da9239356d377870
sunjungAn <dkstjswjd83@naver.com>
2023-07-23 20:17:11 +0900

    feat: code review comment 추가

--- src/component/Avatar/index.d.ts
+++ src/component/Avatar/index.d.ts
@@ -1,5 +1,5 @@
 export type AvatarProps = {
     src?: string;
-    size?: string | number;
-    onClick?: fuction;
+    size?: string | number; //여기 보시면 string 형식으로 두면, 실제로 유저가 사용할 때, size="30px" 이렇게 보낼 수 있다는 의미였습니다. 그렇기 때문에 최대한 제한을 주어 number로만 제한 하는 것이 좋아보입니다.
+    onClick?: fuction; // function 보다는 input, return 값을 명시해주면 좋습니다. 예를 들어 () => void 이런 형태가 되겠네요!
 };
--- src/component/Avatar/index.tsx
+++ src/component/Avatar/index.tsx
@@ -1,25 +1,19 @@
-import { ReactElement } from 'react';
-import { AvatarProps } from './index.d';
-import DefaultProfile from '../Avatar/DefaultProfile.svg'
+import { ReactElement } from "react";
+import { AvatarProps } from "./index.d";
+import DefaultProfile from "../Avatar/DefaultProfile.svg";
 
-export default function Avatar(props: AvatarProps):ReactElement {
-  
-  const { 
-    src = DefaultProfile,
-    size,
-    onClick,
-     } = props;
+export default function Avatar(props: AvatarProps): ReactElement {
+    const { src = DefaultProfile, size, onClick } = props;
 
-  return (
-        <img 
+    return (
+        <img
             src={src}
             onClick={onClick}
-            style={{ 
-                width: `${size}px`,
-                background: '#E9E9E9',
-                borderRadius: '50%',
-              }}  
-              />
-
-  );
+            style={{
+                width: `${size}px`, //만약 유저가 size = '30px'로 줄경우, '30pxpx'가 됩니다.
+                background: "#E9E9E9",
+                borderRadius: "50%",
+            }}
+        />
+    );
 }
