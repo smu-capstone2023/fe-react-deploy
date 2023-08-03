f5c2ac53f761810e74b8af976e5ec6626111e791
sunjungAn <dkstjswjd83@naver.com>
2023-08-04 01:16:38 +0900

    feat: added Pagination, TextField component

---
+++ src/component/Pagination/PaginationItem/index.d.ts
@@ -0,0 +1,10 @@
+export type PaginationItemProps = {
+    color?: "primary" | "gray";
+    shape?: "circular" | "rounded";
+    size?: "small" | "medium" | "large";
+    selected?: boolean;
+    sx?: object;
+    hide?: boolean;
+    page?: number;
+    onClick?: function;
+};
