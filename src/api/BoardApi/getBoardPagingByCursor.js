4b32364670754c2963f4c41ebcd3fdc0712a17ce
hyun <jjjtoy@naver.com>
2023-08-06 03:01:06 +0900

    #452 [Fix]: api 이름, 내용 수정(두 api가 서로 뒤섞여있었던 문제 해결)

--- src/api/BoardApi/getBoardPagingByCursor.js
+++ src/api/BoardApi/getBoardPagingByCursor.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
-export const getBoardPagingByCursor = (board_id, now_page, per_page, keyword, sorting) => {
+export const getBoardPagingByCursor = (board_id, last_id, per_page, keyword, sorting) => {
     return axios
-        .get(`${process.env.REACT_APP_SERVER_URL}/board/paging?board_id=${board_id}&now_page=${now_page}&per_page=${per_page}&keyword=${keyword}`, {
+        .get(`${process.env.REACT_APP_SERVER_URL}board/cursor?board_id=${board_id}&last_id=${last_id}&per_page=${per_page}&keyword=${keyword}`, {
             headers: {
                 Authorization: localStorage.getItem('access_token'),
                 sorting : sorting,
