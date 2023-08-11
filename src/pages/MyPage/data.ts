c4f25b6b0f36f98feca3f95f48038fe2f0b2f518
차다인 <cdin23@naver.com>
2023-08-11 22:26:29 +0900

    feat: #471 mbti 선택 버튼 변경

--- src/pages/MyPage/data.ts
+++ src/pages/MyPage/data.ts
@@ -1,3 +1,4 @@
 export const setting_list: string[] = ["학과인증", "MBTI 변경", "비밀번호 변경", "로그아웃", "탈퇴하기"];
 export const app_info_list: string[] = ["개인정보 처리방침", "앱 버전"];
 export const my_util_list: string[] = ["내가 쓴 글", "내가 좋아요 한 글"];
+export const mbti_list :string[] = ["ISTJ", "ISTP", "INFJ", "INTJ", "ISFJ", "ISFP", "INFP", "INTP", "ESTJ", "ESFP", "ENFP", "ENTP", "ESFJ", "ESTP", "ENFJ", "ENTJ"];
