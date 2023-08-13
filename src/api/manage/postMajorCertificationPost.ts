a49b0d4451526d96596d95c22f9745fbfb7963e7
sunjungAn <dkstjswjd83@naver.com>
2023-08-13 15:34:43 +0900

    feat: manageApi change axios to custom instance

---
+++ src/api/manage/postMajorCertificationPost.ts
@@ -0,0 +1,17 @@
+import { instance } from "api/interceptor";
+
+export const postMajorCertificationPost = (imageUrl: string, content: string) => {
+    console.log(imageUrl, content);
+    return instance
+        .post(`/manage/certificate/create`, {
+            image_url: imageUrl,
+            content: content,
+        })
+        .then((response: any) => {
+            return response.data.isSuccess;
+        })
+        .catch((error: any) => {
+            console.error("postMajorCertificationPost", error);
+            return false;
+        });
+};
