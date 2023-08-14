094b3b82ff53d0ab2568933f4da970b70fdb754e
차다인 <cdin23@naver.com>
2023-08-14 13:52:33 +0900

    feat: #484 시간표 api 추가 및 기능추가

---
+++ src/pages/MyPage/ScheduleImgUpload.tsx
@@ -0,0 +1,67 @@
+import React, { useRef } from "react";
+import { uploadImageToServer } from "../../api/utils/imageUploader";
+import { changeTimeTable } from "../../api/User/ChangeTimeTable";
+
+/**
+ * @param title : string
+ * @param onClickImageButton : () => {}
+ * @returns
+ */
+
+export const ScheduleImgUpload = ({title} :any) => {
+    const fileInput = useRef<HTMLInputElement | null>(null);
+
+    const handleGetImageUrl = (event :any) => {
+        const file = event.target.files[0];
+        const formData = new FormData();
+        formData.append("image", file);
+        uploadImageToServer(formData).then((response) => {
+            if (response !== "") {
+                scheduleImageUpload(response.imageUrls);
+            }
+        });
+    };
+
+    const handleClick = () => {
+        if (fileInput.current !== null) {
+            fileInput.current.click();
+        }
+    };
+
+    const scheduleImageUpload = (imageUrl :string) => {
+        changeTimeTable(imageUrl).then((response)=>{
+            if (response) {
+                alert("이미지가 변경되었습니다");
+                window.location.reload();
+            } else {
+                alert("이미지가 변경에 실패했습니다. 다시 시도해주세요.");
+                window.location.reload();
+            }
+        })
+
+    };
+
+    return (
+        <p style={{
+            fontFamily: "nexon-regular",
+            fontSize: "13px",
+            color: "#4169E1",
+            margin: "3px 0 5px 15px",
+            verticalAlign: "middle",
+            flex: "1",
+        }}
+            onClick={handleClick}
+        >
+            <input
+                type="file"
+                accept="image/jpg, image/jpeg, image/png"
+                style={{ display: "none" }}
+                ref={fileInput}
+                onChange={handleGetImageUrl}
+            />
+        {title}
+        </p>
+    );
+}
+
+export default ScheduleImgUpload;
