b13e1b7d236fd57963c98683fc4f95ade4b1a737
차다인 <cdin23@naver.com>
2023-08-20 16:41:36 +0900

    feat: #493 useState 삭제 및 hover 수정

--- src/pages/Home/Profile.tsx
+++ src/pages/Home/Profile.tsx
@@ -9,16 +9,6 @@ interface ProfileProp {
     major?: string;
 }
 export default function Profile({ nickname, mbti, major }: ProfileProp) {
-    const [isHovering, setIsHovering] = useState<boolean>(false);
-
-    const handleMouseOver = () => {
-        setIsHovering(true);
-      };
-    
-      const handleMouseOut = () => {
-        setIsHovering(false);
-      };
-
     return (
         <div
             css={css`
@@ -66,13 +56,13 @@ export default function Profile({ nickname, mbti, major }: ProfileProp) {
                             css={css`
                                 font-size: 16px;
                                 font-family: nexon-regular;
-                                text-decoration-line: ${isHovering ? 'underline' : 'none'};
+                                &:hover {
+                                    text-decoration-line : underline;
+                                }
                             `}
                             onClick={()=>{
                                 window.location.href="/major-certification";
                             }}
-                            onMouseOver={handleMouseOver}
-                            onMouseOut={handleMouseOut}
                         >
                             {major ? major : "학과인증을 해주세요"}
                         </p>
