8bcdfeb85a0f6e28e05a44cf8b190065dd531805
SunjungAn <55094745+sunjungAn@users.noreply.github.com>
2023-08-24 21:00:00 +0900

    Revert "[Fix] #498  id중복 alert"

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import SignUpView from "../component/template/SignUpView";
 import { signUpSite } from "../api/User/SignUpSite";
-import axios from "axios";
 
 const Signup = () => {
     const [id, setId] = useState("");
@@ -19,7 +18,6 @@ const Signup = () => {
         //Ex) qwer1234! (숫자,문자 필수, 특수문자 허용, 8~16자리)
         return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+]{8,16}$/.test(password);
     };
-    
 
     const checkRePasswordExp = () => {
         if (password === rePassword) {
@@ -29,18 +27,7 @@ const Signup = () => {
         }
     };
 
-    //아이디 중복 체크
-    const checkIdDuplicate = async (id) => {
-        try {
-            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/auth/check-duplicate-id/${id}`);
-            return response.data.is_duplicate;
-        } catch (error) {
-            console.error(error);
-            return true;
-        }
-    };
-
-    const onClickSignupButton = async () => {
+    const onClickSignupButton = () => {
         if (checkIdRegExp() === false) {
             alert("ID 형식을 확인해주세요.");
         } else if (nickname === "") {
@@ -50,21 +37,13 @@ const Signup = () => {
         } else if (checkRePasswordExp() === false) {
             alert("비밀번호가 일치하지 않습니다.");
         } else {
-            const isIdDuplicate = await checkIdDuplicate(id);
-            if (isIdDuplicate) {
-                alert("이미 가입한 아이디입니다.");
-            } else {
-                try {
-                    const response = await signUpSite(id, nickname, password);
-                    if (response) {
-                        window.location.href = "/user-certification";
-                    } else {
-                        alert("네트워크 문제! 잠시 후에 다시 시도해주세요!");
-                    }
-                } catch (error) {
-                    alert("오류가 발생했습니다. 잠시 후에 다시 시도해주세요.");
+            signUpSite(id, nickname, password).then((response) => {
+                if (response) {
+                    window.location.href = "/user-certification";
+                } else {
+                    alert("네트워크 문제! 잠시후에 다시 시도해주세요! ");
                 }
-            }
+            });
         }
     };
 
