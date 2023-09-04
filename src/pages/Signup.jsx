e296f9a06e79bda0646c5dcbbf56af090b3ba746
seyoung <aras3640@gmail.com>
2023-09-04 17:11:46 +0900

    [fix] 회원가입_input 후 커서 활성화

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import SignUpView from "../component/template/SignUpView";
 import { signUpSite } from "../api/User/signUpSite";
 
@@ -7,8 +7,12 @@ const Signup = () => {
     const [password, setPassword] = useState("");
     const [rePassword, setRePassword] = useState("");
     const [nickname, setNickname] = useState("");
-    //임시 값 부여
-
+ 
+    // cursor 활성화용 ref
+    const idInputRef = useRef(null);
+    const passwordInputRef = useRef(null);
+    const rePasswordInputRef = useRef(null);
+    
     const checkIdRegExp = () => {
         //Ex) 123456789(9자리 숫자'만')
         return /^[0-9]{9}$/.test(id);
@@ -28,14 +32,20 @@ const Signup = () => {
     };
 
     const onClickSignupButton = () => {
+        let invalidField = null;
+
         if (checkIdRegExp() === false) {
             alert("ID 형식을 확인해주세요.");
+            invalidField = idInputRef;
         } else if (nickname === "") {
             alert("닉네임을 입력해주세요.");
+            invalidField = null;    //닉네임은 상관x
         } else if (checkPasswordExp() === false) {
             alert("비밀번호 형식을 확인해주세요.");
+            invalidField = passwordInputRef;
         } else if (checkRePasswordExp() === false) {
             alert("비밀번호가 일치하지 않습니다.");
+            invalidField = rePasswordInputRef;
         } else {
             signUpSite(id, nickname, password).then((response) => {
                 if (response) {
@@ -44,6 +54,10 @@ const Signup = () => {
                     alert("네트워크 문제! 잠시후에 다시 시도해주세요! ");
                 }
             });
+        }
+        // 형식 안맞는 input으로 cursor 활성화
+        if (invalidField) {
+            invalidField.current.focus();
         }
     };
 
@@ -54,6 +68,9 @@ const Signup = () => {
             onChangePassword={setPassword}
             onChangeRePassword={setRePassword}
             onClickSignupButton={onClickSignupButton}
+            idInputRef={idInputRef}
+            passwordInputRef={passwordInputRef}
+            rePasswordInputRef={rePasswordInputRef}
         />
     );
 };
