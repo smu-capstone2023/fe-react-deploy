b9cb3e0271745be807a196dc38a0f54801bcfa6f
차다인 <cdin23@naver.com>
2023-09-05 19:34:04 +0900

    feat: 예외처리 return null

--- src/api/User/signUpSite.ts
+++ src/api/User/signUpSite.ts
@@ -13,11 +13,13 @@ export const signUpSite = (school_id: string, nickname: string, password: string
             }
         })
         .catch((error) => {
-            if (error.response.status === 400) {
-                alert("이미 가입된 사용자입니다.");
-            }
             if (error.response.status === 406) {
                 alert("해당 닉네임은 사용할 수 없습니다. 다른 닉네임을 사용해주세요");
+                return null;
+            }
+            if (error.response.status === 400) {
+                alert("이미 가입된 사용자입니다.");
+                return null;
             }
             else {
                 console.error("signupSite", error);
